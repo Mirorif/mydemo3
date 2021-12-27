08f171a40c64247046d367655bfa4c80cfb8fbad
Mirorif <mirzokirovmirorif@gmail.com>
2021-12-26 23:50:05 -0800

    good

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  static String id = "home_page";
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("PDP Tutorial"));
+  }
+}
